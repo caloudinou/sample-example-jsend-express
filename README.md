83e66d8b79e947c6a44ec117911396100dbda715
pascal <pascalou@gmail.com>
2019-03-10 22:05:05 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+## simple implementation to jsend-express
---
+++ routes/example.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const express = require('express')
+const example = express.Router()
+
+class InvalidFields extends Error {
+  constructor (fields) {
+    super('invalid fields')
+    fields = fields || []
+  }
+}
+
+example.get('/success', (req, res, next) => {
+  const data = {
+    example: 'test example'
+  }
+  res.success({ data })
+})
+
+example.get('/partial', (req, res, next) => {
+  const payload = {paginationCount: 2, paginationPage: 1, paginationLimit: 20}
+  const data = {
+    example: 'test example'
+  }
+  res.partial({ payload, data })
+})
+
+example.get('/fail', (req, res, next) => {
+  const error = new InvalidFields([
+    { key: 'inputA', value: 'is required'},
+    { key: 'inputB', value: 'is required'}
+  ])
+  res.fail({ error })
+})
+
+example.get('/error', (req, res, next) => {
+  const error = new Error('error server foo')
+  res.error({ error })
+})
+
+module.exports = datasource
